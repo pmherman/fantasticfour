02ba8564aad848085bdfdcfdc8d27a38e9caac04
Paul M Herman <>
2017-12-14 21:19:19 -0500

    upload

--- assets/ajax.js
+++ assets/ajax.js
@@ -3,6 +3,16 @@ var startDate;
 var endDate;
 var page;
 console.log(query);
+
+$("#search").click(function {
+    var searchTerms = $("#searchTerms").val();
+    var startYear = $("#startYear").val();
+    var endYear = $("#endYear").val();
+    var recordsNumber = $("#recordsNumber").val();
+})
+
+
+
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     queryURL += '?' + $.param({
         'api-key': "af53452d509244bf9e15faf3ed6bccd8",
@@ -18,5 +28,8 @@ var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
         .done(function(response) {
           console.log(response);
           
+          var info = $("<div>");
+
+          var info.addclass("info");
           
         });
